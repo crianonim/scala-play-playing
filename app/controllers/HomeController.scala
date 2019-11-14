00ab27e353ac747a56c5e02281bc1dda38410c41
Jan Skowronski <crianonim@gmail.com>
2019-11-14 17:41:14 +0000

    Add newGame route

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,29 +7,54 @@ import site.jans.screept._
 import site.jans.game._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  val operators=MathOperators.operators++LogicOperators.operators++BasicOperators.operators
-  val initialCtx=Map("dialog"->"start","turn"->"1")
-  val s1=Scenario("pierwszy scenariusz",Scenario.readScenarioFile("scenario.txt"),operators,initialCtx)
-  val gs=GameServer(List(s1))
-  val gameId=gs.startGame(0)
-  val p1=gs.getGame(gameId)
+class HomeController @Inject()(cc: ControllerComponents)
+    extends AbstractController(cc) {
+  val operators = MathOperators.operators ++ LogicOperators.operators ++ BasicOperators.operators
+  val initialCtx = Map("dialog" -> "start", "turn" -> "1")
+  val s1 = Scenario(
+    "pierwszy scenariusz",
+    Scenario.readScenarioFile("scenario.txt"),
+    operators,
+    initialCtx
+  )
+  val gs = GameServer(List(s1))
+  val gameId = gs.startGame(0)
+  val p1 = gs.getGame(gameId)
+
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
-  {
-    println("REQUEST:",request)
-    println(p1)
-    Ok(views.html.index())
+    {
+      println("REQUEST:", request)
+      // println(p1)
+      Ok(views.html.index())
+    }
   }
+  def startGame(scenario_id: String) = Action {
+    implicit request: Request[AnyContent] =>
+      {
+        println("SID", scenario_id)
+        val gameId=gs.startGame(scenario_id.toInt)
+        Ok("GameId"+gameId)
+      }
+
+  }
+  def show(scenario_id: String) = Action {
+    implicit request: Request[AnyContent] =>
+      {
+        println("SID", scenario_id)
+        val gameId=gs.startGame(scenario_id.toInt)
+        Ok("GameId"+gameId)
+      }
+
   }
 }
