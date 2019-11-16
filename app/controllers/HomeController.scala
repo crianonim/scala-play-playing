d0d2b101082ae70a050c8199b13d857247aa373f
Jan Skowronski <crianonim@gmail.com>
2019-11-16 11:38:38 +0000

    Update mapToJSON to escape newlines

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,10 +2,11 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.http._
 import play.api.mvc._
 import site.jans.screept._
 import site.jans.game._
-
+import akka.util._
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
@@ -32,12 +33,21 @@ class HomeController @Inject()(cc: ControllerComponents)
   val p1 = gs.getGame(gameId)
 
   def mapToJSON(m: Map[String, Any]): String = {
+    def wrap(x:Any):Any =x match {
+      case x: Int  => x
+      case x: Boolean  => x
+      case x: Seq[Any] => x map wrap mkString("[ ",", "," ]")
+      case x: String  => s""" "${x.replaceAll("\n","\\\\n")}" """
+      case _ => ""
+    }
     val result = (for {
       (k, v) <- m
-      val str = v match {
-        case x: Int => s""" "${k}": ${x}"""
-        case x      => s""" "${k}": "${x}" """
-      }
+      val str = s""" "${k}": ${wrap(v)} """
+      // val str = v match {
+      //   case x: Int => s""" "${k}": ${x}"""
+      //   case x: Vector[String] => s""" "${k}":   ${x.mkString("[",",","]")} """
+      //   case x      => s""" "${k}": "${x}" """
+      // }
     } yield str)
     // println(result.mkString(","))
     "{\n" + result.mkString(", ") + "\n}"
@@ -54,6 +64,9 @@ class HomeController @Inject()(cc: ControllerComponents)
     {
       println(mapToJSON(initialCtx))
       println(mapToJSON(Map("id" -> 12, "name" -> "Jan")))
+      println(mapToJSON(Map("id" -> 12, "name" -> "Jan","jj"->List(1,2,3))))
+      println(mapToJSON(Map("id" -> 12, "name" -> "Jan","jj"->List("1",2,true,"3"))))
+
       println("REQUEST:", request)
       val scenarios = gs.getScenarios()
       val playthroughs = gs.getStartedGames()
@@ -91,7 +104,7 @@ class HomeController @Inject()(cc: ControllerComponents)
       {
         println("SID", scenario_id)
         val gameId = gs.startGame(scenario_id.toInt)
-        Ok("GameId" + gameId)
+        Ok(mapToJSON(Map("game_id"->gameId)))
       }
 
   }
@@ -99,7 +112,11 @@ class HomeController @Inject()(cc: ControllerComponents)
     implicit request: Request[AnyContent] =>
       {
         val playthrough = gs.getGame(gameId.toInt)
-        Ok("Game" + playthrough.show())
+        val response=playthrough.show() match {
+          case (head,intro,options)=>Map("head"->head,"intro"->intro,"options"->options)
+        }
+        Result(ResponseHeader(200,Map.empty[String,String]),HttpEntity.Strict(ByteString(mapToJSON(response)),Some("application/json")))
+        // Ok(mapToJSON(response))
       }
 
   }
