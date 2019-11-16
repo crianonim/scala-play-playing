424d9ee89a01944008b3d8241d10404d095a937e
Jan Skowronski <crianonim@gmail.com>
2019-11-16 19:05:05 +0000

    Added enableCors method and change format of APIgetGames

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,7 @@ import play.api.mvc._
 import site.jans.screept._
 import site.jans.game._
 import akka.util._
+
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
@@ -14,6 +15,17 @@ import akka.util._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents)
     extends AbstractController(cc) {
+
+  implicit class RichResult(result: Result) {
+    def enableCors = result.withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "OPTIONS, GET, POST, PUT, DELETE, HEAD" // OPTIONS for pre-flight
+      ,
+      "Access-Control-Allow-Headers" -> "Accept, Content-Type, Origin, X-Json, X-Prototype-Version, X-Requested-With" //, "X-My-NonStd-Option"
+      ,
+      "Access-Control-Allow-Credentials" -> "true"
+    )
+  }
   val operators = Screept.getCoreOperators()
   val initialCtx = Map("dialog" -> "start", "turn" -> "1")
   val s1 = Scenario(
@@ -33,24 +45,29 @@ class HomeController @Inject()(cc: ControllerComponents)
   val p1 = gs.getGame(gameId)
 
   def mapToJSON(m: Map[String, Any]): String = {
-    def wrap(x:Any):Any =x match {
-      case x: Int  => x
+    def wrap(x: Any): Any = x match {
+      case x: Int      => x
       case x: Boolean  => x
-      case x: Seq[Any] => x map wrap mkString("[ ",", "," ]")
-      case x: String  => s""" "${x.replaceAll("\n","\\\\n")}" """
-      case _ => ""
+      case x: Seq[Any] => x map wrap mkString ("[ ", ", ", " ]")
+      case x: String   => s""" "${x.replaceAll("\n", "\\\\n")}" """
+      case x: Map[String,Any] => mapToJSON(x)
+      case _           => ""
     }
     val result = (for {
       (k, v) <- m
       val str = s""" "${k}": ${wrap(v)} """
-     
+
     } yield str)
     "{\n" + result.mkString(", ") + "\n}"
   }
 
-  def respondWithJSON(s:String)= {
-    Result(ResponseHeader(200,Map.empty[String,String]),HttpEntity.Strict(ByteString(s),Some("application/json")))
+  def respondWithJSON(s: String) = {
+    Result(
+      ResponseHeader(200, Map.empty[String, String]),
+      HttpEntity.Strict(ByteString(s), Some("application/json"))
+    )
   }
+
   /**
     * Create an Action to render an HTML page.
     *
@@ -62,8 +79,14 @@ class HomeController @Inject()(cc: ControllerComponents)
     {
       println(mapToJSON(initialCtx))
       println(mapToJSON(Map("id" -> 12, "name" -> "Jan")))
-      println(mapToJSON(Map("id" -> 12, "name" -> "Jan","jj"->List(1,2,3))))
-      println(mapToJSON(Map("id" -> 12, "name" -> "Jan","jj"->List("1",2,true,"3"))))
+      println(
+        mapToJSON(Map("id" -> 12, "name" -> "Jan", "jj" -> List(1, 2, 3)))
+      )
+      println(
+        mapToJSON(
+          Map("id" -> 12, "name" -> "Jan", "jj" -> List("1", 2, true, "3"))
+        )
+      )
 
       println("REQUEST:", request)
       val scenarios = gs.getScenarios()
@@ -72,15 +95,13 @@ class HomeController @Inject()(cc: ControllerComponents)
       Ok(views.html.index(scenarios, playthroughs))
     }
   }
- 
-  
 
   def APIstartGame(scenario_id: String) = Action {
     implicit request: Request[AnyContent] =>
       {
         println("SID", scenario_id)
         val gameId = gs.startGame(scenario_id.toInt)
-        respondWithJSON(mapToJSON(Map("game_id"->gameId)))
+        respondWithJSON(mapToJSON(Map("game_id" -> gameId)))
       }
 
   }
@@ -88,24 +109,31 @@ class HomeController @Inject()(cc: ControllerComponents)
     implicit request: Request[AnyContent] =>
       {
         val playthrough = gs.getGame(gameId.toInt)
-        val response=playthrough.show() match {
-          case (head,intro,options)=>Map("head"->head,"intro"->intro,"options"->options)
+        val response = playthrough.show() match {
+          case (head, intro, options) =>
+            Map("head" -> head, "intro" -> intro, "options" -> options)
         }
         respondWithJSON(mapToJSON(response))
       }
 
   }
-  def APIgetGames() = Action {
-    implicit request: Request[AnyContent] =>
-      {
-        respondWithJSON(mapToJSON(gs.getStartedGames().map(pair=>(pair._1+"",pair._2)).toMap))
-      }
+  def APIgetGames() = Action { implicit request: Request[AnyContent] =>
+    {
+      respondWithJSON(
+        mapToJSON(
+          // gs.getStartedGames().map(pair => (pair._1 + "", pair._2)).toMap
+          Map("games"->gs.getStartedGames().map(pair => Map("id"->pair._1 , "scenarioTitle"->pair._2)))
+
+        )
+      ).enableCors
+    }
   }
-  def APIgetScenarios() = Action {
-    implicit request: Request[AnyContent] =>
-      {
-        respondWithJSON(mapToJSON(gs.getScenarios().map(pair=>(pair._1+"",pair._2)).toMap))
-      }
+  def APIgetScenarios() = Action { implicit request: Request[AnyContent] =>
+    {
+      respondWithJSON(
+        mapToJSON(gs.getScenarios().map(pair => (pair._1 + "", pair._2)).toMap)
+      )
+    }
   }
   def APIplayOption(gameId: String, option: String) = Action {
     implicit request: Request[AnyContent] =>
@@ -113,8 +141,9 @@ class HomeController @Inject()(cc: ControllerComponents)
         val playthrough = gs.getGame(gameId.toInt)
         playthrough.play(option.toInt)
 
-         val response=playthrough.show() match {
-          case (head,intro,options)=>Map("head"->head,"intro"->intro,"options"->options)
+        val response = playthrough.show() match {
+          case (head, intro, options) =>
+            Map("head" -> head, "intro" -> intro, "options" -> options)
         }
         respondWithJSON(mapToJSON(response))
       }
